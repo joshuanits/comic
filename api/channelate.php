747f1b66a786c71759759ace7a50c24681ea3783
Joshua N <joshuanits@gmail.com>
2018-09-09 04:16:53 +0000

    Finished cyanide and channelate apis

---
+++ api/channelate.php
@@ -0,0 +1,62 @@
+<?php
+if(!array_key_exists('id', $_GET)){
+    exit(400);
+}
+
+$id = $_GET["id"];
+
+$url = "http://www.channelate.com";
+if($id != 0){
+    $url = "http://channelate.com/comic/$id";
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+if(curl_errno($ch))
+    echo 'Curl error: '.curl_error($ch);
+curl_close ($ch);  
+
+
+$resp = array();
+
+try {
+    $doc = new DOMDocument();
+    $doc->loadHTML($response);
+
+    $headerTags = $doc->getElementsByTagName('h2');
+    $header = $headerTags[0];
+    if($header->getAttribute('class') == "post-title") {
+        $resp['link'] = $header->firstChild->getAttribute('href');
+    }
+
+    $spanTags = $doc->getElementsByTagName('span');
+    foreach($spanTags as $span){
+        if($span->getAttribute('class') == "comic-tall") { 
+            $resp['img'] = $span->firstChild->getAttribute('src');
+            $resp['title'] = $span->firstChild->getAttribute('title');
+        }
+    }
+    
+    $bonusdiv = $doc->getElementById('extrapanelbutton');
+    $resp['bonus'] = $bonusdiv->childNodes[1]->getAttribute('href');
+
+    if(array_key_exists('link', $resp)){
+        $parts = explode("/", $resp['link']);
+        $resp['id'] = $parts[count($parts) - 2];
+    }
+
+    #$resp['imgUrl'] = $doc->getElementById('main-comic')->getAttribute('src');
+
+} catch (HttpException $ex) {
+    $resp["success"] = 0;
+    $resp["error"] = $ex;
+}
+
+header('Content-Type: application/json');
+echo json_encode($resp);
+
+
+?>
--- api/cyanide.php
+++ api/cyanide.php
@@ -5,12 +5,23 @@ if(!array_key_exists('id', $_GET)){
 
 $id = $_GET["id"];
 
-$url = "www.explosm.net";
+
+     
+
+$url = "http://explosm.net";
 if($id != 0){
-    $url = "www.explosm.net/comics/$id";
+    $url = "http://explosm.net/comics/$id";
 }
 
-$reponse = http_get($url);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+if(curl_errno($ch))
+    echo 'Curl error: '.curl_error($ch);
+curl_close ($ch);  
+
 
 $resp = array();
 
@@ -18,13 +29,15 @@ try {
     $doc = new DOMDocument();
     $doc->loadHTML($response);
     $resp['id'] = $doc->getElementById('comic-social-favorite')->getAttribute('data-id');
-    $resp['imgUrl'] = $doc->getElementById('main-comic')->getAttribute('src');
-    $resp['perm'] = "explosm.net/comics/" + $resp['id'];
+    $resp['img'] = $doc->getElementById('main-comic')->getAttribute('src');
+    $id = $resp['id'];
+    $resp['link'] = "explosm.net/comics/$id";
 } catch (HttpException $ex) {
     $resp["success"] = 0;
     $resp["error"] = $ex;
 }
 
+header('Content-Type: application/json');
 echo json_encode($resp);
 
 
